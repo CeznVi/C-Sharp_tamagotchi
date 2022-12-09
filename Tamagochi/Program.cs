fb799b83c46b1dc92da7143ec6e3ca4b7df76be9
Viktor <Cezn.Vi@gmail.com>
2022-12-09 23:26:56 +0200

    finish

--- Tamagochi/Program.cs
+++ Tamagochi/Program.cs
@@ -1,18 +1,168 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Tamagochi
 {
     internal class Program
     {
+        public delegate string Delegate();
+
+        static System.Timers.Timer aTimer = new System.Timers.Timer(10000);
+
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.Unicode;
+            
+            Random random = new Random();
+            List<int> list = new List<int>();
 
+            //Кількість життів у зайки
+            int life = 3;
 
+            //Тривалість життя тамагочі у мілі секундах (1хв = 60 000 мілісекунд)
+            int lifeLenght = 60000;
+
+            //Кількість команд
+            int count = 25;
+            string name = "Тамагочі Зайка";
+
+            Tamagochi zaika = new Tamagochi();
+            //Таймери
+            Stopwatch lifeCycle = new Stopwatch();
+
+            Stopwatch st = new Stopwatch();
+            Stopwatch st0 = new Stopwatch();
+
+            Delegate[] d = new Delegate[]
+            {
+                zaika.Eat,
+                zaika.Play,
+                zaika.Sleap,
+                zaika.Walk,
+                zaika.Heal
+            };
+            Delegate method = null;
+            
+
+            int temp = random.Next(d.Length);
+            list.Add(temp);
+
+            //Генерація бажань зайки
+            while(list.Count < count)
+            {
+                temp = random.Next(d.Length);
+                if (temp != list[0])
+                {
+                    temp = random.Next(d.Length);
+                    list.Add(temp);
+                }
+            }
+
+            lifeCycle.Start();
+            while (life > 0 && lifeCycle.ElapsedMilliseconds < lifeLenght)
+            {
+                
+                int it  = 0;
+                Console.Clear();
+                zaika.Show();
+                Console.WriteLine($"\nКількість життів = {life}\n");
+
+                while (life > 1 && lifeCycle.ElapsedMilliseconds < lifeLenght)
+                {
+                    Thread.Sleep(800);
+                    Console.Clear();
+
+                    int index = list[it];
+
+                    method = d[index];
+                    string message = method();
+
+                    if (message == zaika.Eat()) zaika.ShowWantEat();
+                    else if(message == zaika.Walk()) zaika.ShowWantWalk();
+                    else if (message == zaika.Sleap()) zaika.ShowWantSleap();
+                    else if (message == zaika.Play()) zaika.ShowWantPlay();
+                    else if (message == zaika.Heal()) zaika.ShowWantHeal();
+
+                    Console.WriteLine($"\nКількість життів = {life}\n");
+
+                    st.Start();
+                    var result = MessageBox.Show(message, name, MessageBoxButtons.OKCancel);
+                    if (result == DialogResult.OK) 
+                    {
+                        st.Stop();
+                        if (st.ElapsedMilliseconds > 10000)
+                        {
+                            life--;
+                            Console.WriteLine("Поки ви думали. Зайка захворів. -1 життя. Їй боляче");
+                            Console.WriteLine($"Життів залишилось {life}");
+                        }
+                        else
+                            MessageBox.Show("Дякую!", name, MessageBoxButtons.OK);
+                        st.Reset();
+                    }
+                    else
+                    {
+                        life--;
+                        st.Stop();
+                        Console.WriteLine("Зайка захворів. -1 життя. Їй боляче");
+                        Console.WriteLine($"Життів залишилось {life}");
+                        MessageBox.Show(":(", name, MessageBoxButtons.OK);
+                        st.Reset();
+                        Thread.Sleep(800);
+                    }
+
+                    it++;
+
+                    if (it >= list.Count) 
+                        it = 0; 
+                }
+                if(life < 3)
+                {
+                    Console.Clear();
+                    zaika.ShowWantHeal();
+                    st0.Start();
+                    var needHeal = MessageBox.Show(d[4](), name, MessageBoxButtons.OKCancel);
+                    if (needHeal == DialogResult.OK)
+                    {
+                        st0.Stop();
+                        if (st0.ElapsedMilliseconds > 5000)
+                        {
+                            life = 0;
+                            Console.Clear();
+                            zaika.Death();
+                            Console.WriteLine($"{name} помер поки ви думали...");
+                            break;
+                        }
+
+                        MessageBox.Show("Ооо дякую!", name, MessageBoxButtons.OK);
+                        if (life < 3)
+                           life++;
+                        st0.Reset();
+                    }
+                    else
+                    {
+                        st0.Stop();
+                        life = 0;
+                        Console.Clear();
+                        Console.WriteLine("Зайка загинув.");
+                        zaika.Death();
+                        Console.ReadKey();
+                    }
+                    st0.Reset();
+                }
+            }
+            lifeCycle.Stop();
+            if (lifeCycle.ElapsedMilliseconds >= lifeLenght)
+            {
+                Console.Clear();
+                Console.WriteLine("Зайка прожила з вами щасливе життя.");
+            }
         }
     }
 }
--- Tamagochi/tamagochi.cs
+++ Tamagochi/tamagochi.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,7 @@ namespace Tamagochi
         public string Sleap() { return "Я хочу спати"; }
         public string Heal() { return "Вилікуй мене"; }
         public string Play() { return "Давай пограємось"; }
-        public void avatar()
+        public void Show()
         {
             Console.WriteLine("______§§____________§§");
             Console.WriteLine("_____§__§__________§__§");
@@ -40,6 +41,156 @@ namespace Tamagochi
             Console.WriteLine("____§____§§§_____§§§____§");
             Console.WriteLine("_____§§§§___§§§§§___§§§§");
         }
+        public void Death()
+        {
+            Console.WriteLine("░░░░░░░░░░░░▄▄░░░░░░░░░░░░");
+            Console.WriteLine("░░░░░░░░░░▄████▄░░░░░░░░░░");
+            Console.WriteLine("░░░░░░░░░████████░░░░░░░░░");
+            Console.WriteLine("░░░░░░░░░▀██████▀░░░░░░░░░");
+            Console.WriteLine("░░░░░░░░░░░████░░░░░░░░░░░");
+            Console.WriteLine("░░▄███▄░░░░████░░░░▄███▄░░");
+            Console.WriteLine("▄████████████████████████▄");
+            Console.WriteLine("▀████████████████████████▀");
+            Console.WriteLine("░░▀███▀░░░░████░░░░▀███▀░░");
+            Console.WriteLine("░░░░░░░░░░░████░░░░░░░░░░░");
+            Console.WriteLine("░░░░░░░░░░░████░░░░░░░░░░░");
+            Console.WriteLine("░░░░░░░░░░░████░░░░░░░░░░░");
+            Console.WriteLine("░░░░░░░░░░░████░░░░░░░░░░░");
+            Console.WriteLine("░░░░░░░░░░░████░░░░░░░░░░░");
+            Console.WriteLine("░░░░░░░░░░░████░░░░░░░░░░░");
+            Console.WriteLine("░░░░░░░░░▄██████▄░░░░░░░░░");
+            Console.WriteLine("░░░░░░░░░████████░░░░░░░░░");
+            Console.WriteLine("░░░░░░░░░░▀████▀░░░░░░░░░░");
+
+        }
+
+        public void ShowWantEat()
+        {
+            Console.WriteLine("______§§____________§§");
+            Console.WriteLine("_____§__§__________§__§");
+            Console.WriteLine("___§§____§_________§___§");
+            Console.WriteLine("__§___§___§_______§____§");
+            Console.WriteLine("_§____§____§_____§____§");
+            Console.WriteLine("_§___§_§____§___§____§           |==========|");
+            Console.WriteLine("_§§§§___§___§§§§§___§            |ХОЧУ ЇСТИ!|");
+            Console.WriteLine("_________§_________§___§§_§§     |==========|");
+            Console.WriteLine("________§___§§_§§___§_§§§c§§§====|");
+            Console.WriteLine("_______§_____________§_§d§§§");
+            Console.WriteLine("_______§______§______§__§§§");
+            Console.WriteLine("_______§____§___§____§___§");
+            Console.WriteLine("________§____§§§____§___§");
+            Console.WriteLine("_______§§§§§_____§§§§§_§");
+            Console.WriteLine("_§§§__§_____§§§§§_____§__§§§");
+            Console.WriteLine("§_§_§§____§_______§__§_§§_§_§");
+            Console.WriteLine("§____§_____§_____§__§__§____§");
+            Console.WriteLine("_§____§____§_____§_§__§____§");
+            Console.WriteLine("__§____§§§§_______§§§§____§");
+            Console.WriteLine("___§____§___________§____§");
+            Console.WriteLine("____§____§§§_____§§§____§");
+            Console.WriteLine("_____§§§§___§§§§§___§§§§");
+        }
+        public void ShowWantWalk()
+        {
+            Console.WriteLine("______§§____________§§");
+            Console.WriteLine("_____§__§__________§__§");
+            Console.WriteLine("___§§____§_________§___§");
+            Console.WriteLine("__§___§___§_______§____§");
+            Console.WriteLine("_§____§____§_____§____§ ");
+            Console.WriteLine("_§___§_§____§___§____§           |=============|");
+            Console.WriteLine("_§§§§___§___§§§§§___§            |Пішли гуляти!|");
+            Console.WriteLine("_________§_________§___§§_§§     |=============|");
+            Console.WriteLine("________§___§§_§§___§_§§§c§§§====|");
+            Console.WriteLine("_______§_____________§_§d§§§");
+            Console.WriteLine("_______§______§______§__§§§");
+            Console.WriteLine("_______§____§___§____§___§");
+            Console.WriteLine("________§____§§§____§___§");
+            Console.WriteLine("_______§§§§§_____§§§§§_§");
+            Console.WriteLine("_§§§__§_____§§§§§_____§__§§§");
+            Console.WriteLine("§_§_§§____§_______§__§_§§_§_§");
+            Console.WriteLine("§____§_____§_____§__§__§____§");
+            Console.WriteLine("_§____§____§_____§_§__§____§");
+            Console.WriteLine("__§____§§§§_______§§§§____§");
+            Console.WriteLine("___§____§___________§____§");
+            Console.WriteLine("____§____§§§_____§§§____§");
+            Console.WriteLine("_____§§§§___§§§§§___§§§§");
+        }
+        public void ShowWantSleap()
+        {
+            Console.WriteLine("______§§____________§§");
+            Console.WriteLine("_____§__§__________§__§");
+            Console.WriteLine("___§§____§_________§___§");
+            Console.WriteLine("__§___§___§_______§____§");
+            Console.WriteLine("_§____§____§_____§____§ ");
+            Console.WriteLine("_§___§_§____§___§____§           |=============|");
+            Console.WriteLine("_§§§§___§___§§§§§___§            | Хочу спати! |");
+            Console.WriteLine("_________§_________§___§§_§§     |=============|");
+            Console.WriteLine("________§___§§_§§___§_§§§c§§§====|");
+            Console.WriteLine("_______§_____________§_§d§§§");
+            Console.WriteLine("_______§______§______§__§§§");
+            Console.WriteLine("_______§____§___§____§___§");
+            Console.WriteLine("________§____§§§____§___§");
+            Console.WriteLine("_______§§§§§_____§§§§§_§");
+            Console.WriteLine("_§§§__§_____§§§§§_____§__§§§");
+            Console.WriteLine("§_§_§§____§_______§__§_§§_§_§");
+            Console.WriteLine("§____§_____§_____§__§__§____§");
+            Console.WriteLine("_§____§____§_____§_§__§____§");
+            Console.WriteLine("__§____§§§§_______§§§§____§");
+            Console.WriteLine("___§____§___________§____§");
+            Console.WriteLine("____§____§§§_____§§§____§");
+            Console.WriteLine("_____§§§§___§§§§§___§§§§");
+        }
+        public void ShowWantPlay()
+        {
+            Console.WriteLine("______§§____________§§");
+            Console.WriteLine("_____§__§__________§__§");
+            Console.WriteLine("___§§____§_________§___§");
+            Console.WriteLine("__§___§___§_______§____§");
+            Console.WriteLine("_§____§____§_____§____§ ");
+            Console.WriteLine("_§___§_§____§___§____§           |=============|");
+            Console.WriteLine("_§§§§___§___§§§§§___§            |Хочу гратись!|");
+            Console.WriteLine("_________§_________§___§§_§§     |=============|");
+            Console.WriteLine("________§___§§_§§___§_§§§c§§§====|");
+            Console.WriteLine("_______§_____________§_§d§§§");
+            Console.WriteLine("_______§______§______§__§§§");
+            Console.WriteLine("_______§____§___§____§___§");
+            Console.WriteLine("________§____§§§____§___§");
+            Console.WriteLine("_______§§§§§_____§§§§§_§");
+            Console.WriteLine("_§§§__§_____§§§§§_____§__§§§");
+            Console.WriteLine("§_§_§§____§_______§__§_§§_§_§");
+            Console.WriteLine("§____§_____§_____§__§__§____§");
+            Console.WriteLine("_§____§____§_____§_§__§____§");
+            Console.WriteLine("__§____§§§§_______§§§§____§");
+            Console.WriteLine("___§____§___________§____§");
+            Console.WriteLine("____§____§§§_____§§§____§");
+            Console.WriteLine("_____§§§§___§§§§§___§§§§");
+        }
+        public void ShowWantHeal()
+        {
+            Console.WriteLine("______§§____________§§");
+            Console.WriteLine("_____§__§__________§__§");
+            Console.WriteLine("___§§____§_________§___§");
+            Console.WriteLine("__§___§___§_______§____§");
+            Console.WriteLine("_§____§____§_____§____§ ");
+            Console.WriteLine("_§___§_§____§___§____§           |=============|");
+            Console.WriteLine("_§§§§___§___§§§§§___§            |Полікуй мене!|");
+            Console.WriteLine("_________§_________§___§§_§§     |=============|");
+            Console.WriteLine("________§___§§_§§___§_§§§c§§§====|");
+            Console.WriteLine("_______§_____________§_§d§§§");
+            Console.WriteLine("_______§______§______§__§§§");
+            Console.WriteLine("_______§____§___§____§___§");
+            Console.WriteLine("________§____§§§____§___§");
+            Console.WriteLine("_______§§§§§_____§§§§§_§");
+            Console.WriteLine("_§§§__§_____§§§§§_____§__§§§");
+            Console.WriteLine("§_§_§§____§_______§__§_§§_§_§");
+            Console.WriteLine("§____§_____§_____§__§__§____§");
+            Console.WriteLine("_§____§____§_____§_§__§____§");
+            Console.WriteLine("__§____§§§§_______§§§§____§");
+            Console.WriteLine("___§____§___________§____§");
+            Console.WriteLine("____§____§§§_____§§§____§");
+            Console.WriteLine("_____§§§§___§§§§§___§§§§");
+        }
+
+
     }
 
 
